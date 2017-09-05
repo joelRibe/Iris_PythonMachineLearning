2ec8572ececcf5cb4bd0863c70358f04c36e0243
Joel Oliveira Ribeiro <joel.oliver.ribe@gmail.com>
2017-09-05 19:20:36 -0300

    Adding new Files nea_neighborhodd

--- distEuclidian.py
+++ distEuclidian.py
@@ -1,7 +1,10 @@
 import math
 import numpy as np
 
-def dist_euclidiana_np(v1, v2):
+
+def dist_euclidiana_np(v1, v2,index):
+	v1=list(map(float,v1[:index]))
+	v2=list(map(float,v2[:index]))
 	v1, v2 = np.array(v1), np.array(v2)
 	diff = v1 - v2
 	quad_dist = np.dot(diff, diff)
--- load_data.py
+++ load_data.py
@@ -1,28 +1,37 @@
 import numpy as np
 
-dataset = np.genfromtxt('iris.txt',delimiter=' ', dtype='str')
+def load_data(fileName):
 
-setosas = np.where( dataset == 'Setosa')[0].tolist()
-versicolors = np.where( dataset == 'Versicolor')[0].tolist()
-virginicas = np.where( dataset == 'Virginica')[0].tolist()
+	dataset = np.genfromtxt(fileName,delimiter=' ', dtype='str')
 
-
-np.random.shuffle(setosas)
-np.random.shuffle(versicolors)
-np.random.shuffle(virginicas)
+	setosas = np.where( dataset == 'Setosa')[0].tolist()
+	versicolors = np.where( dataset == 'Versicolor')[0].tolist()
+	virginicas = np.where( dataset == 'Virginica')[0].tolist()
 
 
+	np.random.shuffle(setosas)
+	np.random.shuffle(versicolors)
+	np.random.shuffle(virginicas)
 
-trainingSet = []
-testSet = []
 
-for index in range(0,50):
-    if index <= 39:
-        trainingSet.append(dataset[setosas[index]].tolist())
-        trainingSet.append(dataset[versicolors[index]].tolist())
-        trainingSet.append(dataset[virginicas[index]].tolist())
-    else:
-        testSet.append(dataset[setosas[index]].tolist())
-        testSet.append(dataset[versicolors[index]].tolist())
-        testSet.append(dataset[virginicas[index]].tolist())
+
+	trainingSet = []
+	testSet = []
+
+	for index in range(0,50):
+		if index <= 39:
+			trainingSet.append(dataset[setosas[index]].tolist())
+			trainingSet.append(dataset[versicolors[index]].tolist())
+			trainingSet.append(dataset[virginicas[index]].tolist())
+		else:
+			testSet.append(dataset[setosas[index]].tolist())
+			testSet.append(dataset[versicolors[index]].tolist())
+			testSet.append(dataset[virginicas[index]].tolist())
+	return [trainingSet,testSet]
+
+
+
+
+
+
 
