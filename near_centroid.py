ea98037789e9881d6560510185b6e78d384892b9
Joel Oliveira Ribeiro <joel.oliver.ribe@gmail.com>
2017-09-06 15:49:31 -0300

    Updtating the centroid files

--- near_centroid.py
+++ near_centroid.py
@@ -1,12 +1,23 @@
-from distEuclidian import dist_euclidiana_py
-
+import numpy as np
+from distEuclidian import dist_euclidiana_np
+from vectorCentroid import vector_centroid
+from load_data import load_data # for Test
 
 def centroides(trainingSet):
 	
-	# setosaCentroid = 
+	# setosaCentroid = vector_centroid(np.where( trainingSet == 'Setosa')[:4] , len(trainingSet[0]))
 
-	# versicolorCentroid =
+	# versicolorCentroid = vector_centroid(np.where( trainingSet == 'Versicolor')[:4], len(trainingSet[0]))
 
-	# virginicaCentroid = 
+	# virginicaCentroid = vector_centroid(np.where( trainingSet == 'Virginica')[:4], len(trainingSet[0]))
 
 	return [setosaCentroid,versicolorCentroid,virginicaCentroid]
+
+
+#testing the 'centroides' function
+# trainingSet,testSet = load_data('iris.txt') 
+# centroides = centroides(trainingSet)
+
+# print(centroides[0])
+# print(centroides[1])
+# print(centroides[2])
