78d5b49f68971b4c2b7907aa55fad009ee6891f0
Joel Oliveira Ribeiro <joel.oliver.ribe@gmail.com>
2017-09-21 13:39:23 -0300

    Initializing normalization method

---
+++ normalization_data.py
@@ -0,0 +1,11 @@
+# Method : Constant Norm
+# this simple method of normalization consists in maiintaining constants and equals "one" the norm of vectors
+# that a dataset .
+from load_data import *
+import numpy as np
+
+
+def data_normalization(dataset):
+    dataset_normalized = [] * range(dataset)
+
+    return dataset_normalized
